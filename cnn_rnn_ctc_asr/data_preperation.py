7be29d6c71b5d7a3b968e53ed33a233e8d934c24
Steven Grima <steven.grima@behavox.com>
2025-08-11 22:27:32 -0400

    feat: train scripts

--- cnn_rnn_ctc_asr/data_preperation.py
+++ cnn_rnn_ctc_asr/data_preperation.py
@@ -11,10 +11,12 @@ a great starting point for ASR tasks with a manageable size.
 It includes functions to load audio files, extract features, and prepare the dataset.
 """
 
-
-import logging
-from lhotse.recipes.audio_mnist import prepare_audio_mnist
 import argparse
+import logging
+
+from lhotse.recipes.audio_mnist import prepare_audio_mnist
+from lhotse import RecordingSet, SupervisionSet, CutSet
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -27,18 +29,38 @@ logger.addHandler(console_handler)
 
 def prepare_audiomnist(data_dir, output_dir):
     """
-    Prepares the AudioMNIST dataset for ASR by loading audio files and extracting features.
-    
+    Prepares the AudioMNIST dataset for ASR by loading audio files and
+    extracting features.
+
     Args:
-        data_dir (str): Directory where the AudioMNIST dataset is stored. (The repo root directory)
+        data_dir (str): Directory where the AudioMNIST dataset is stored.
+        (The repo root directory)
         output_dir (str): Directory where the prepared dataset will be saved.
     """
 
-    # Load the AudioMNIST dataset
-    manifests = prepare_audio_mnist(
-        corpus_dir=data_dir,
-        output_dir=output_dir,
-    )
+    # if supervisions.to_file(output_dir / "audio_mnist_supervisions.jsonl.gz")
+    # recordings.to_file(output_dir / "audio_mnist_recordings.jsonl.gz")
+    # exists just load them from disk
+
+    supervision_file = output_dir / "audio_mnist_supervisions.jsonl.gz"
+    recordings_file = output_dir / "audio_mnist_recordings.jsonl.gz"
+
+    if recordings_file.is_file() and supervision_file.is_file():
+        logger.info("Loading existing AudioMNIST dataset...")
+        recordings = RecordingSet.from_file(recordings_file)
+        supervisions = SupervisionSet.from_file(supervision_file)
+        manifests = {
+            "recordings": recordings,
+            "supervisions": supervisions
+        }
+        logger.info("AudioMNIST dataset loaded successfully.")
+    else:
+        logger.info("Preparing AudioMNIST dataset...")
+        # Load the AudioMNIST dataset
+        manifests = prepare_audio_mnist(
+            corpus_dir=data_dir,
+            output_dir=output_dir,
+        )
 
     return manifests
 
@@ -61,3 +83,15 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     manifests = prepare_audiomnist(args.data_dir, args.output_dir)
+
+    # Lets take this a bit further and convert it into a CutSet and then split
+    cuts = CutSet.from_manifests(
+        recordings=manifests["recordings"],
+        supervisions=manifests["supervisions"]
+    )
+
+    train_cuts, test_cuts = cuts.split(num_splits=2, proportions=[0.9, 0.1])
+    train_cuts.to_file(args.output_dir / "audio-mnist_cuts_train.jsonl.gz")
+    test_cuts.to_file(args.output_dir / "audio-mnist_cuts_test.jsonl.gz")
+
+    logger.info("AudioMNIST dataset prepared and saved to %s", args.output_dir)

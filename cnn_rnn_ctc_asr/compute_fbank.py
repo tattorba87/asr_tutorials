50ecb02b0a15ab0d7b054ac93e550c4b14fba6ef
Steven Grima <steven.grima@behavox.com>
2025-08-11 21:40:47 -0400

    feat: train scripts

--- cnn_rnn_ctc_asr/compute_fbank.py
+++ cnn_rnn_ctc_asr/compute_fbank.py
@@ -100,7 +100,7 @@ def compute_fbank_audiomnist(
                         + cut_set.perturb_speed(0.9)
                         + cut_set.perturb_speed(1.1)
                     )
-            
+
             cut_set = cut_set.compute_and_store_features(
                 extractor=extractor,
                 storage_path=f"{output_dir}/{prefix}_feats_{partition}",

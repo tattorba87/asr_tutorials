a3e89122f53d822af3502c8b89d4f7fa66c37f20
Steven Grima <steven.grima@behavox.com>
2025-08-13 20:53:57 -0400

    feat: update data sync

--- cnn_rnn_ctc_asr/compute_fbank.sh
+++ cnn_rnn_ctc_asr/compute_fbank.sh
@@ -3,21 +3,22 @@
 set -euo pipefail
 
 source ./path.sh
+n_jobs=8
 
-python ./compute_fbank.py \
+python compute_fbank.py \
   --src-dir ~/data/AudioMNIST_processed \
   --output-dir ~/data/AudioMNIST_processed/fbank \
   --partition train \
   --perturb-speed true \
-  --num-jobs 64 \
+  --num-jobs $n_jobs \
   --num-mel-bins 80
 
-python ./compute_fbank.py \
+python compute_fbank.py \
   --src-dir ~/data/AudioMNIST_processed \
   --output-dir ~/data/AudioMNIST_processed/fbank \
   --partition test \
   --perturb-speed true \
-  --num-jobs 64 \
+  --num-jobs $n_jobs \
   --num-mel-bins 80
 
 echo "Fbank features computed and saved to ~/data/AudioMNIST_processed/fbank"
